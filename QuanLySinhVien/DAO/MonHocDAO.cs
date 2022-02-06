ab2dd290afe373ce83b2eb6325a9f63581b48c09
nguyenThanhDg <nguyenthanhdungktm@gmail.com>
2022-02-06 07:06:22 +0700

    MonHoc

---
+++ QuanLySinhVien/DAO/MonHocDAO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLySinhVien.DTO;
+
+namespace QuanLySinhVien.DAO
+{
+    class MonHocDAO
+    {
+        private static MonHocDAO instance;
+        internal static MonHocDAO Instance
+        {
+            get { if (instance == null) instance = new MonHocDAO(); return MonHocDAO.instance; }
+            private set { instance = value; }
+        }
+
+        public DataTable getMonHocs()
+        {
+            string query = "SELECT * FROM dbo.MONHOC";
+
+            return DataProvider.Instance.ExecuteQuery(query);
+        }
+
+        public bool insertMonHoc(string tenMonHoc, int soTinChi, int maKhoa)
+        {
+            string query = string.Format("INSERT dbo.MONHOC( TENMONHOC, SOTINCHI, MAKHOA ) VALUES (N'{0}', N'{1}', N'{2}')", tenMonHoc, soTinChi, maKhoa);
+
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool updateMonHoc(int maMonHoc, string tenMonHoc, int soTinChi, int maKhoa)
+        {
+            string query = string.Format("UPDATE dbo.MONHOC SET TENMONHOC = N'{1}', SOTINCHI = N'{2}', MAKHOA=N'{3}' WHERE MAMonHoc = {0}", maMonHoc, tenMonHoc, soTinChi, maKhoa);
+
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool deleteMonHoc(int maMonHoc)
+        {
+            string query = string.Format("DELETE dbo.MONHOC WHERE MAMONHOC = {0}", maMonHoc);
+
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public List<MonHoc> SearchMonHocByName(string name)
+        {
+            List<MonHoc> list = new List<MonHoc>();
+
+            string query = string.Format("SELECT * FROM dbo.MONHOC WHERE dbo.fuConvertToUnsign1(TENMONHOC) LIKE N'%' + dbo.fuConvertToUnsign1(N'{0}') + '%'", name);
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                MonHoc MonHoc = new MonHoc(item);
+                list.Add(MonHoc);
+            }
+
+            return list;
+        }
+    }
+}
+
---
+++ QuanLySinhVien/DTO/MonHoc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace QuanLySinhVien.DTO
+{
+    class MonHoc
+    {
+        public MonHoc(int maMonHoc, string tenMonHoc, int soTinChi, int maKhoa)
+        {
+            this.maMonHoc = maMonHoc;
+            this.tenMonHoc = tenMonHoc;
+            this.soTinChi = soTinChi;
+            this.maKhoa = maKhoa;
+        }
+
+        public MonHoc(DataRow row)
+        {
+            this.maMonHoc = (int)row["maMonHoc"];
+            this.tenMonHoc = row["tenMonHoc"].ToString();
+            this.soTinChi = (int)row["soTinChi"];
+            this.maKhoa = (int)row["maKhoa"];
+        }
+
+        private int maMonHoc;
+
+        public int MaMonHoc
+        {
+            get { return maMonHoc; }
+            set { maMonHoc = value; }
+        }
+
+        private string tenMonHoc;
+
+        public string TenMonHoc
+        {
+            get { return tenMonHoc; }
+            set { TenMonHoc = value; }
+        }
+        private int soTinChi;
+
+        public int SoTinChi
+        {
+            get { return soTinChi; }
+            set { soTinChi = value; }
+        }
+        private int maKhoa;
+
+        public int MaKhoa
+        {
+            get { return maKhoa; }
+            set { maKhoa = value; }
+        }
+
+    }
+}
+
